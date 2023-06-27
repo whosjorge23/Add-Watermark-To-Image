1894b2d3c7d593fad1fc5c1a90364bb1498bf047
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-06-27 19:43:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from PIL import Image
 import os
 
-def add_logo_to_images(images_folder, logo_path, output_dir):
-    # Caricare il logo e convertirlo in RGBA se ha trasparenza
-    logo = Image.open(logo_path)
-    if logo.mode in ('P', 'RGB'):
-        logo = logo.convert('RGBA')
+def add_logo_to_images(images_folder, logo_path_right, logo_path_left, output_dir):
+    # Caricare il logo destro e convertirlo in RGBA se ha trasparenza
+    logo_right = Image.open(logo_path_right)
+    if logo_right.mode in ('P', 'RGB'):
+        logo_right = logo_right.convert('RGBA')
     
-    # Ridurre le dimensioni del logo del 50%
-    logo_size = (int(logo.width * 0.2), int(logo.height * 0.2))
-    logo = logo.resize(logo_size, Image.ANTIALIAS)
+    # Caricare il logo sinistro e convertirlo in RGBA se ha trasparenza
+    logo_left = Image.open(logo_path_left)
+    if logo_left.mode in ('P', 'RGB'):
+        logo_left = logo_left.convert('RGBA')
+    
+    # Ridurre le dimensioni dei loghi del 50%
+    logo_size_right = (int(logo_right.width * 0.2), int(logo_right.height * 0.2))
+    logo_right = logo_right.resize(logo_size_right, Image.ANTIALIAS)
+
+    logo_size_left = (int(logo_left.width * 0.2), int(logo_left.height * 0.2))
+    logo_left = logo_left.resize(logo_size_left, Image.ANTIALIAS)
 
     # Ottenere la lista di tutti i file nella cartella delle immagini
     image_files = os.listdir(images_folder)
@@ -22,30 +30,34 @@ def add_logo_to_images(images_folder, logo_path, output_dir):
         # Aprire l'immagine
         image = Image.open(image_path)
 
-        # Utilizzare il canale alfa del logo come maschera
-        logo_mask = logo.split()[-1] if logo.mode == 'RGBA' else None
+        # Utilizzare il canale alfa del logo destro come maschera
+        logo_mask_right = logo_right.split()[-1] if logo_right.mode == 'RGBA' else None
+        
+        # Utilizzare il canale alfa del logo sinistro come maschera
+        logo_mask_left = logo_left.split()[-1] if logo_left.mode == 'RGBA' else None
         
         # Calcolare la posizione in cui inserire il logo in alto a destra e inserirlo
-        position_top_right = (image.width - logo.width, 0)
-        image_with_logo = image.copy()
-        image_with_logo.paste(logo, position_top_right, logo_mask)
+        position_top_right = (image.width - logo_right.width, 0)
+        image_with_logos = image.copy()
+        image_with_logos.paste(logo_right, position_top_right, logo_mask_right)
 
         # Calcolare la posizione in cui inserire il logo in alto a sinistra e inserirlo
-        #position_top_left = (0, 0)
-        #image_with_logo.paste(logo, position_top_left, logo_mask)
+        position_top_left = (0, 0)
+        image_with_logos.paste(logo_left, position_top_left, logo_mask_left)
 
         # Salvare l'immagine modificata in una cartella di output
         output_path = os.path.join(output_dir, os.path.basename(image_path))
-        image_with_logo.save(output_path)
+        image_with_logos.save(output_path)
 
 # Cartella contenente le immagini
-images_folder = 'images'
+images_folder = 'jpg'
 
-# Percorso del file logo
-logo_path = 'logo/logo.png'
+# Percorsi dei file loghi
+logo_path_right = 'logo/logo_right.png'
+logo_path_left = 'logo/logo_left.png'
 
 # Cartella in cui salvare le immagini modificate
 output_dir = 'output/'
 
 # Eseguire la funzione
-add_logo_to_images(images_folder, logo_path, output_dir)
+add_logo_to_images(images_folder, logo_path_right, logo_path_left, output_dir)
